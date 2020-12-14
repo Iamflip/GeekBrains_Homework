8840928abed7a8fdfd23b611e4ba54cc7775fdcf
Rodion <kim.Radik@mail.ru>
2020-12-14 19:28:00 +0600

    Updated Homework-1

---
+++ Homework-1/Homework-1/Homework-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}]");
+            Console.ReadLine();
+        }
+    }
+}
