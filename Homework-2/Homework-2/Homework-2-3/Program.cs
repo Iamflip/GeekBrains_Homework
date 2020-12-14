031803ec1667368b3308aca2ef57aa653077b56c
Rodion <kim.Radik@mail.ru>
2020-12-14 20:34:08 +0600

    Updated Homework-2

---
+++ Homework-2/Homework-2/Homework-2-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine($"Число {number} чётное");
+            else
+                Console.WriteLine($"Число {number} не чётное");
+            Console.ReadLine();
+        }
+    }
+}
