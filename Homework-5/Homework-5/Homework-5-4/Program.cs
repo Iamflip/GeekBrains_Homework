8e73cb335e2fe02c3e4d2a17d340b7e96e5f1754
Rodion <kim.Radik@mail.ru>
2020-12-25 02:48:33 +0600

    Homework-5

---
+++ Homework-5/Homework-5/Homework-5-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5_4
+{
+    class Program
+    {
+        //Я пытался но голова уже не варит, к следующему дз постараюсь сделать
+        static void Main(string[] args)
+        {
+            CheckFiles(@"C:\Users\Iamflip\Desktop\GeekBrains_Howework\Homework-5\Homework-5\Homework-5-3");
+        }
+        static string CheckFiles(string directory)
+        {
+            int help = 0;
+            string[] files = Directory.GetFiles(directory);
+            string[] directories = Directory.GetDirectories(directory);
+
+            if (Directory.GetFiles(directory).Length > 0)
+            {
+                CheckFiles(files[help]);
+
+            }
+
+            return files.ToString();
+
+        }
+    }
+}
